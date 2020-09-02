068d534549cfc29c2365baf85470fa41ced7072a
shun takagi <s.takagi.910@gmail.com>
2020-09-02 22:33:21 +0900

    first

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module MyApp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "localhost:3000"
+        origins ENV['CORS_ORIGINS']
         resource "*",
           headers: :any,
           methods: [:get, :post, :options, :head]
