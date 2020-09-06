f24825f6c8f401a46f34799ca50bc79c7b7a3c76
shun takagi <s.takagi.910@gmail.com>
2020-09-07 01:51:34 +0900

    origin設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins %w[https://service-todoapp-front.takagishun.vercel.app http://localhost:3000]
+    origins %w[https://service-todoapp-front.takagishun.vercel.app https://service-todoapp-front.vercel.app http://localhost:3000]
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
