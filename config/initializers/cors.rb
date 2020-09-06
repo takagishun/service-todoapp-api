a7b7669b6a27b6ac3796c24bd054a6711d157716
shun takagi <s.takagi.910@gmail.com>
2020-09-06 23:56:48 +0900

    TODOリスト

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS'] || 'localhost:3000'
+    origins ENV['CORS_ORIGINS']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
