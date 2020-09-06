c479be1a70adfadb4dcd991b50222b69817d86c3
shun takagi <s.takagi.910@gmail.com>
2020-09-07 03:54:45 +0900

    redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,5 +102,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.session_store :redis_store, servers: ENV['REDIS_URL'], expire_after: 1.day
 end
